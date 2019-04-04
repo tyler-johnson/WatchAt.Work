540771af6fcb6b42b39cdd6aaf499191d9c37edf
Tyler Johnson <tyler@tylerjohnson.me>
2019-04-04 12:07:30 -0600

    add custom icons and rename

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
+import "./index.css";
 import * as React from "react";
+import ReactDOM from "react-dom";
+import { Player, useLoadTwitchEmbedJS } from "./player";
 import { Provider } from "./state";
 import { Header } from "./header";
-import { Player, useLoadTwitchEmbedJS } from "./player";
-// import { Chat } from "./chat";
 
 export const App: React.FC = function() {
   return (
@@ -25,15 +26,4 @@ export const Body: React.FC = function() {
   return <Player />;
 };
 
-// import React from 'react';
-import ReactDOM from "react-dom";
-import "./index.css";
-// import App from './App';
-import * as serviceWorker from "./serviceWorker";
-
 ReactDOM.render(<App />, document.getElementById("root"));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
